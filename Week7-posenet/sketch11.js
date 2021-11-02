fe19f4a6049674c005a76601e2e5221688f9eb0c
ZhengZhou1024 <79291679+ZhengZhou1024@users.noreply.github.com>
2021-11-02 19:01:58 -0400

    Add files via upload

---
+++ Week7-posenet/sketch11.js
@@ -0,0 +1,61 @@
+// ml5.js: Pose Estimation with PoseNet
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/Courses/ml5-beginners-guide/7.1-posenet.html
+// https://youtu.be/OIo-DIOkNVg
+// https://editor.p5js.org/codingtrain/sketches/ULA97pJXR
+
+let video;
+let poseNet;
+let pose;
+let skeleton;
+
+function setup() {
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses) {
+    //console.log(poses); 
+    if (poses.length > 0) {
+        pose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+    }
+}
+
+
+function modelLoaded() {
+    console.log('poseNet ready');
+}
+
+function draw() {
+    image(video, 0, 0);
+
+    if (pose) {
+        let eyeR = pose.rightEye;
+        let eyeL = pose.leftEye;
+        let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+        fill(222, 13, 34);
+        ellipse(pose.nose.x, pose.nose.y, d);
+        fill(212, 0, 255);
+        ellipse(pose.rightWrist.x, pose.rightWrist.y, 35);
+        ellipse(pose.leftWrist.x, pose.leftWrist.y, 35);
+
+        for (let i = 0; i < pose.keypoints.length; i++) {
+            let x = pose.keypoints[i].position.x;
+            let y = pose.keypoints[i].position.y;
+            fill(227, 23, 13);
+            ellipse(x, y, 20, 20);
+        }
+
+        for (let i = 0; i < skeleton.length; i++) {
+            let a = skeleton[i][0];
+            let b = skeleton[i][1];
+            strokeWeight(10);
+            stroke(127, 255, 0);
+            line(a.position.x, a.position.y, b.position.x, b.position.y);
+        }
+    }
+}
